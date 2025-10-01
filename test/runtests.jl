b61eda2f2c94df537123650206556c3d0d9fa2cf
pulsipher <joshuapulsipher13@gmail.com>
2025-10-01 16:14:46 -0400

    resolve test conflicts

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,6 @@ Pkg.add(url="https://github.com/infiniteopt/InfiniteOpt.jl/", rev="master")
 using Test, InfiniteOpt, InfiniteExaModels, MadNLP, NLPModelsIpopt, ExaModels, Ipopt
 
 @testset "Transcription Backend" begin include("transcription.jl") end
-@testset "Finite Parameters" begin include("finite_parameters.jl") end
-@testset "Parameter Function Methods" begin include("parameter_functions.jl") end
 @testset "Solve Tests" begin include("solve.jl") end
 
 # TODO add more tests
